ab84b2c403dc6201a3b1543e188bf1821aaa3d6a
Karlis <karlisstekels.dev@gmail.com>
2023-04-28 09:03:40 +0300

    Initial Commit

---
+++ BankingApp Demo/BankingApp_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankingApp_DemoApp.swift
+//  BankingApp Demo
+//
+//  Created by karlis.stekels on 28/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct BankingApp_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
