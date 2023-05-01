1c0123d685d0ebe29ab2687abba12a940ea7bc15
kstekels <kstekels+github@gmail.com>
2023-05-01 21:03:56 +0300

    not finished

--- BankingApp Demo/Screens/TransferFundsScreen.swift
+++ BankingApp Demo/Screens/TransferFundsScreen.swift
@@ -8,13 +8,93 @@
 import SwiftUI
 
 struct TransferFundsScreen: View {
+    @Environment(\.presentationMode) var presentationMode
+
+    @ObservedObject private var transferFundsVM = TransferFundsViewModel()
+    @State private var showSheet: Bool = false
+    @State private var isFromAccount = false
+    
+    var actionSheetButtons: [Alert.Button] {
+        var actionBtn = self.transferFundsVM.filteredAccounts.map { account in
+            Alert.Button.default(Text("\(account.name) (\(account.accountType))")) {
+                if self.isFromAccount {
+                    self.transferFundsVM.fromAccount = account
+                } else {
+                    self.transferFundsVM.toAccount = account
+                }
+            }
+        }
+        
+        actionBtn.append(.cancel())
+        return actionBtn
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            AccountListVIew(accounts: self.transferFundsVM.accounts).frame(height: 300)
+            
+            TransferFundsAccountSelectionView(transferFundsVM: self.transferFundsVM, showSheet: $showSheet, isFromAccount: $isFromAccount)
+            Spacer()
+            Text(self.transferFundsVM.message ?? "")
+            Button("Submit Transfer") {
+                self.transferFundsVM.submitTransfer()
+                self.presentationMode.wrappedValue.dismiss()
+            }
+            .padding()
+            .buttonStyle(.borderedProminent)
+            .disabled(self.transferFundsVM.toAccount == nil)
+            
+        }
+        .onAppear{
+            self.transferFundsVM.populateAccounts()
+        }
+        .actionSheet(isPresented: $showSheet, content: {
+            ActionSheet(title: Text("Transfer Funds"), message: Text("Choose an account"), buttons: self.actionSheetButtons)
+        })
+        .navigationTitle("Transfer Funds")
+        .embedInNavigationView()
     }
 }
 
 struct TransferFundsScreen_Previews: PreviewProvider {
     static var previews: some View {
         TransferFundsScreen()
+    }
+}
+
+struct TransferFundsAccountSelectionView: View {
+    
+    @ObservedObject var transferFundsVM: TransferFundsViewModel
+    @Binding var showSheet: Bool
+    @Binding var isFromAccount: Bool
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Button("From \(self.transferFundsVM.fromAccounType)") {
+                showSheet = true
+                isFromAccount = true
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.green)
+            .foregroundColor(.white)
+            
+            Button("To \(self.transferFundsVM.toAccounType)") {
+                isFromAccount = false
+                showSheet = true
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .background(.green)
+            .foregroundColor(.white)
+            .opacity(self.transferFundsVM.fromAccount != nil ? 1.0 : 0.5)
+            .disabled(self.transferFundsVM.fromAccount == nil)
+            
+            TextField("Amount", text: self.$transferFundsVM.amount)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            
+            
+        }.padding()
     }
 }
--- BankingApp Demo/View Models/TransferFundsViewModel.swift
+++ BankingApp Demo/View Models/TransferFundsViewModel.swift
@@ -11,8 +11,72 @@ class TransferFundsViewModel: ObservableObject {
     
     var fromAccount: AccountViewModel?
     var toAccount: AccountViewModel?
+    @Published var message: String?
     
     @Published var accounts: [AccountViewModel] = [AccountViewModel]()
+    var amount: String = ""
+    
+    var isAmountValid: Bool {
+        guard let userAmount = Double(amount) else {
+            return false
+        }
+        return userAmount <= 0 ? false : true
+    }
+    
+    var filteredAccounts: [AccountViewModel] {
+        if self.fromAccount == nil {
+            return accounts
+        } else {
+            return accounts.filter {
+                
+                guard let fromAccount = self.fromAccount else {
+                    return false
+                }
+                
+                return $0.accountID != fromAccount.accountID
+                
+            }
+        }
+    }
+    
+    
+    var fromAccounType: String {
+        fromAccount != nil ? fromAccount!.accountType : ""
+    }
+    
+    var toAccounType: String {
+        toAccount != nil ? toAccount!.accountType : ""
+    }
+    
+    private func isValid() -> Bool {
+        return isAmountValid
+    }
+    
+    func submitTransfer() {
+        
+        if isValid() {
+            return
+        }
+        
+        guard let fromAccount = fromAccount,
+              let toAccount = toAccount,
+              let amount = Double(amount)
+        else {
+            return
+        }
+        
+        let transferFundRequest = TransferFundRequest(accountFromId: fromAccount.accountID.lowercased(), accountToId: toAccount.accountID.lowercased(), amount: amount)
+        
+        AccountService.shared.transferFunds(transferFundRequest: transferFundRequest) { result in
+            switch result{
+            case .success(let response):
+                print(response)
+            case .failure(let error):
+                self.message = error.localizedDescription
+            }
+        }
+        
+    }
     
     func populateAccounts() {
         AccountService.shared.getAllAccounts { result in
